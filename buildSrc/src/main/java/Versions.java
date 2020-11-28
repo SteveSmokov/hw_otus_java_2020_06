47d0df5266e1a0c1ab750649fbcd18c39df9bff0
SteveSmokov <steve.smokov@ukr.net>
2020-11-29 01:30:41 +0200

    Create module hw14-mvc

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -1,4 +1,10 @@
 public interface Versions {
     String guava = "30.0-jre";
-
+    String gson = "2.8.6";
+    String reflections = "0.9.11";
+    String h2 = "1.4.200";
+    String hibernate_core = "5.4.1.Final";
+    String spring = "5.2.2.RELEASE";
+    String thymeleaf = "3.0.11.RELEASE";
+    String servlet_api = "4.0.1";
 }
